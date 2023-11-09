a861de619f898e494c79e68293d840bf75be1b3a
crisci <crisciluigi2000@gmail.com>
2023-11-09 14:20:03 +0100

    feat: new style and screenshot and delete

--- src/capture/mod.rs
+++ src/capture/mod.rs
@@ -1,19 +1,23 @@
 use std::thread;
 use std::time::Duration;
-use iced::{Length, theme, color, Application, Command, Element, Renderer, executor, Theme, window};
-use iced::widget::{button, container, column, svg, row, horizontal_space, text};
+use iced::{theme, color, Application, Command, Element, Renderer, executor, Theme, window, Length, alignment, Alignment, ContentFit};
+use iced::alignment::{Horizontal, Vertical};
+use iced::widget::{button, container, column, svg, row, horizontal_space, text, Container, Column, image, Image};
 use iced::window::Mode;
+use screenshots::image::RgbaImage;
 use screenshots::Screen;
 
 #[derive(Default)]
 pub struct Capture {
+    screenshot: Option<RgbaImage>,
     screens: Vec<Screen>
 }
 
 #[derive(Debug, Clone, Copy)]
 pub enum Message {
-    Screenshot(usize),
-    WindowHidden
+    Screenshot,
+    WindowHidden,
+    Drop
 }
 
 impl Application for Capture {
@@ -23,7 +27,7 @@ impl Application for Capture {
     type Flags = ();
 
     fn new(_flags: Self::Flags) -> (Self, Command<Self::Message>) {
-        (Self { screens: Screen::all().unwrap() }, Command::none())
+        (Self { screenshot: None, screens: Screen::all().unwrap() }, Command::none())
     }
 
     fn title(&self) -> String {
@@ -32,80 +36,102 @@ impl Application for Capture {
 
     fn update(&mut self, message: Self::Message) -> Command<Self::Message> {
         return match message {
-            Message::Screenshot(n) => {
-                thread::spawn(move || {
-                    screenshot(n);
-                });
+            Message::Screenshot => {
                 let change_mode = window::change_mode(window::Mode::Hidden);
-                let wait = Command::perform(tokio::time::sleep(std::time::Duration::from_secs(1)), |_| Message::WindowHidden);
+                let wait = Command::perform(tokio::time::sleep(std::time::Duration::from_millis(20)), |_| Message::WindowHidden);
                 Command::batch(vec![change_mode, wait])
             },
             Message::WindowHidden => {
+                screenshot(self);
                 window::change_mode(Mode::Windowed)
+            },
+            Message::Drop => {
+                self.screenshot = None;
+                Command::none()
             }
         };
     }
 
     fn view(&self) ->  Element<'_, Self::Message, Renderer<Self::Theme>> {
-
-        let monitor1 = svg::Handle::from_path(format!(
-            "{}/resources/monitor1.svg",
-            env!("CARGO_MANIFEST_DIR")
-        ));
-        let svg1 = svg(monitor1).width(Length::Fill).height(Length::Fill)
-            .style(theme::Svg::custom_fn(|_theme| svg::Appearance {
-                color: Some(color!(0xffffff)),
-        }));
-        let mut content = row![text("Screenshot")];
-        if self.screens.iter().count() == 1 {
-            content = row![image_button(svg1, 0)]
+        let image: Element<Message> = if let Some(screenshot) = &self.screenshot
+        {
+            image(image::Handle::from_pixels(
+                screenshot.width(),
+                screenshot.height(),
+                screenshot.clone().into_raw(),
+            ))
+                .content_fit(ContentFit::Contain)
+                .width(Length::Fill)
+                .height(Length::Fill)
+                .into()
         } else {
-            let monitor2 = svg::Handle::from_path(format!(
-                "{}/resources/monitor2.svg",
-                env!("CARGO_MANIFEST_DIR")
-            ));
-            let svg2 = svg(monitor2).width(Length::Fill).height(Length::Fill)
-                .style(theme::Svg::custom_fn(|_theme| svg::Appearance {
-                    color: Some(color!(0xffffff)),
-                }));
-            content = row![
-                image_button(svg1, 0),
-                horizontal_space(10),
-                image_button(svg2, 1),
-            ];
+            text("Press the button to take a screenshot!").into()
+        };
+
+        let image = container(image)
+            .padding(10)
+            .width(Length::FillPortion(2))
+            .height(Length::Fill)
+            .center_x()
+            .center_y();
+
+        let mut button_row = row![
+                image_button("screenshot", "Screenshot", Message::Screenshot),
+            ].spacing(10).align_items(Alignment::Center);
+
+        if self.screenshot.is_some() {
+            let delete_button = image_button("delete", "Delete", Message::Drop);
+            button_row = button_row.push(delete_button);
         }
 
+
+        let content = column![
+            image,
+            container(button_row)
+                .align_x(alignment::Horizontal::Center)
+                .width(Length::FillPortion(1))
+                .center_x()
+        ];
         container(
-            column![
-                content
-            ]
+           content
         )
             .width(Length::Fill)
             .height(Length::Fill)
             .padding(10)
             .center_x()
-            .center_y()
             .into()
     }
 
-    fn theme(&self) -> Self::Theme {
-        Theme::Dark
-    }
-
 }
 
-fn screenshot(target_screen: usize) {
+fn screenshot(target: &mut Capture) {
     thread::sleep(Duration::from_millis(500));
     let screens = Screen::all().unwrap();
-    let image = screens[target_screen].capture().unwrap();
-    image.save(format!("./monitor-{}.png", target_screen)).unwrap();
+    let image = screens[0].capture().unwrap();
+    target.screenshot = Some(image);
 }
 
-fn image_button<'a>(image_svg: svg::Svg, target_screen: usize) -> iced::widget::Button<'a, Message, > {
-    button(
-        image_svg
-    ).on_press(Message::Screenshot(target_screen))
-        .style(theme::Button::Primary)
-        .width(65)
-        .height(65)
+fn image_button<'a>(image_name: &'a str, description: &'a str, message: Message) -> Container<'a, Message, Renderer> {
+    let handle = svg::Handle::from_path(format!(
+        "{}/resources/{}.svg",
+        env!("CARGO_MANIFEST_DIR"),
+        image_name
+    ));
+
+    let svg = svg(handle).width(Length::Fill).height(Length::Fill)
+        .style(theme::Svg::custom_fn(|_theme| svg::Appearance {
+            color: Some(color!(0xffffff)),
+        }));
+    let c = column![
+        text(description),
+        container(
+            button(svg)
+            .on_press(message)
+            .style(theme::Button::Primary)
+            .width(65)
+            .height(50)
+        ),
+    ].align_items(Alignment::Center);
+
+    container(c).center_x()
 }
--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,6 @@ use iced::{Settings, window, Application};
 mod capture;
 
 pub fn main() -> iced::Result {
-    let settings: Settings<()> = Settings {
-        window: window::Settings {
-            size: (500, 250),
-            resizable: false,
-            decorations: true,
-            ..Default::default()
-        },
-        ..Default::default()
-    };
-    capture::Capture::run(settings)
+    tracing_subscriber::fmt::init();
+    capture::Capture::run(iced::Settings::default())
 }
