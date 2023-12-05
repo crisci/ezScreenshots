d57747c41cec848d0961ee8e68084a43b80c4fe0
crisci <crisciluigi2000@gmail.com>
2023-12-05 22:14:08 +0100

    fix: button layout and optimization

--- src/app/mod.rs
+++ src/app/mod.rs
@@ -220,15 +220,14 @@ impl Application for App {
             .height(Length::Fill)
             .center_x()
             .center_y();
-
         let screenshot_button = image_button("screenshot", "Screenshot", Message::Screenshot);
-        let drag_button = image_button("drag", "Resize", Message::Resize);
-        let delete_button = image_button("delete", "Delete", Message::Drop);
-        let save_button = image_button("save", "Save", Message::MenuAction(MenuAction::Save));
         let button_row = if self.screenshot.is_some() {
-            row![drag_button].push(delete_button).push(screenshot_button).push(save_button).spacing(10).align_items(Alignment::Center)
+            let drag_button = image_button("drag", "Resize", Message::Resize);
+            let delete_button = image_button("delete", "Delete", Message::Drop);
+            let save_button = image_button("save", "Save", Message::MenuAction(MenuAction::Save));
+            row![drag_button].spacing(10).push(delete_button).spacing(10).push(screenshot_button).spacing(10).push(save_button).align_items(Alignment::Center)
         } else {
-            row![Space::new(55, 55)].push(screenshot_button).spacing(10).align_items(Alignment::Center)
+            row![Space::new(55, 55)].spacing(10).push(screenshot_button).align_items(Alignment::Center)
         };
         let mut bottom_container = Row::new()
             .push(match self.save_state {
@@ -250,9 +249,9 @@ impl Application for App {
                 .height(30)
                 .width(30)
                 .content_fit(ContentFit::Contain);
-            bottom_container = bottom_container.push(container(delay_svg).height(55).width(55).padding(15).center_x().center_y());
+            bottom_container = bottom_container.spacing(10).push(container(delay_svg).height(55).width(55).padding(15).center_x().center_y());
         } else {
-            bottom_container = bottom_container.push(Space::new(55, 55)).spacing(10).align_items(Alignment::Center);
+            bottom_container = bottom_container.spacing(10).push(Space::new(55, 55)).align_items(Alignment::Center);
         }
 
         let body = column![
